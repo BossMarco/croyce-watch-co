8712bf9a6e2e82c06cc2c2d27ad53285cae37bee
BossMarco <129328448+BossMarco@users.noreply.github.com>
2026-08-29 05:40:44 -0500

    Add components/WatchAtmosphere.tsx

---
+++ components/WatchAtmosphere.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { PredictiveArcCanvas } from "@designcodeio/threeui/components/PredictiveArcCanvas";
+
+export function WatchAtmosphere() {
+  return <div className="watch-atmosphere" aria-hidden="true"><PredictiveArcCanvas variant="amber-halftone" /></div>;
+}
